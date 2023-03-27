af239c9ecb3088313d06f5ed5d3d61ce48d3c6ed
Cristian Ceballos <cmceballos88@gmail.com>
2023-03-28 01:10:58 +0200

    REmove comentaries optimize js start with product section of the shop section

--- index.js
+++ index.js
@@ -5,6 +5,10 @@ function init() {
   tabContainer.addEventListener("click", function (event) {
     const target = event.target;
     if (target.tagName === "LI") {
+      const active = target.classList.contains("active");
+      if (active) {
+        return;
+      }
       const liElements = tabContainer.querySelectorAll("ul li");
       for (const element of liElements) {
         element.classList.remove("active");
